83ba22450d9d3dfb934bcec61a75791bcc15f058
JaviSerrano <jsermar1@epsg.upv.es>
2024-10-08 20:04:20 +0200

    YA VA ALELUYA

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 // Función para obtener los datos de los sensores
 async function fetchSensorData() {
     try {
-        const response = await fetch('http://localhost:13000/latest'); // Actualiza aquí a 13000
+        const response = await fetch('http://localhost:13000/latest');
         const data = await response.json();
 
         // Actualiza el HTML con los datos de CO2 y temperatura
-        document.getElementById('ozonoValue').textContent = data.co2 ? data.co2.value : 'No data'; // Cambia "ozono" a "co2"
+        document.getElementById('valorOzono').textContent = data.co2 ? data.co2.value : 'No data'; // Cambia "ozono" a "co2"
         document.getElementById('ozonoTimestamp').textContent = data.co2 ? new Date(data.co2.timestamp).toLocaleString() : 'No data';
-        document.getElementById('temperatureValue').textContent = data.temperature ? data.temperature.value : 'No data'; // Asegúrate de mostrar los valores correctamente
+        document.getElementById('valorTemperatura').textContent = data.temperature ? data.temperature.value : 'No data'; // Asegúrate de mostrar los valores correctamente
         document.getElementById('temperatureTimestamp').textContent = data.temperature ? new Date(data.temperature.timestamp).toLocaleString() : 'No data';
 
     } catch (error) {
@@ -15,5 +15,12 @@ async function fetchSensorData() {
     }
 }
 
-// Llama a la función cuando se carga el script
-fetchSensorData();
+
+
+window.onload = () => {
+    fetchSensorData();
+    setInterval(fetchSensorData, 1000);
+}
+
+
+
